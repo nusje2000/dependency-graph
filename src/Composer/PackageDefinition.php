b8faf793652eec8c4c84d7ea2f45cb28ce3cf053
nusje2000 <maarten.nusteling@gmail.com>
2020-01-05 00:46:56 +0100

    Added license and functions to PackageDefinition

--- src/Composer/PackageDefinition.php
+++ src/Composer/PackageDefinition.php
@@ -35,6 +35,20 @@ final class PackageDefinition
         return new PackageDefinition($definition, $file->getPath());
     }
 
+    public static function createFromDirectory(string $path): self
+    {
+        $pathToComposerFile = $path . DIRECTORY_SEPARATOR . '/composer.json';
+        $contents = file_get_contents($pathToComposerFile);
+
+        if (!is_string($contents)) {
+            throw new DefinitionException(sprintf('Could not read contents of file "%s".', $pathToComposerFile));
+        }
+
+        $definition = json_decode($contents, true);
+
+        return new PackageDefinition($definition, $path);
+    }
+
     public function getName(): string
     {
         $name = $this->definition['name'] ?? null;
@@ -59,11 +73,82 @@ final class PackageDefinition
         return $this->definition['require'] ?? [];
     }
 
+    public function hasDependency(string $dependency): bool
+    {
+        return isset($this->definition['require'][$dependency]);
+    }
+
+    public function setDependency(string $dependency, string $versionConstraint): void
+    {
+        $this->definition['require'][$dependency] = $versionConstraint;
+    }
+
+    public function removeDependency(string $dependency): void
+    {
+        unset($this->definition['require'][$dependency]);
+
+        if (empty($this->definition['require'])) {
+            unset($this->definition['require']);
+        }
+    }
+
+    public function getDependencyVersionConstraint(string $dependency): string
+    {
+        $constraint = $this->definition['require'][$dependency] ?? null;
+
+        if (!is_string($constraint)) {
+            throw new DefinitionException(sprintf('Could not get version constraint for dependency "%s".', $dependency));
+        }
+
+        return $constraint;
+    }
+
     /**
      * @return array<string, string>
      */
     public function getDevDependencies(): array
     {
         return $this->definition['require-dev'] ?? [];
+    }
+
+    public function hasDevDependency(string $dependency): bool
+    {
+        return isset($this->definition['require-dev'][$dependency]);
+    }
+
+    public function addDevDependency(string $dependency, string $versionConstraint): void
+    {
+        $this->definition['require-dev'][$dependency] = $versionConstraint;
+    }
+
+    public function removeDevDependency(string $dependency): void
+    {
+        unset($this->definition['require-dev'][$dependency]);
+
+        if (empty($this->definition['require-dev'])) {
+            unset($this->definition['require-dev']);
+        }
+    }
+
+    public function getDevDependencyVersionConstraint(string $dependency): string
+    {
+        $constraint = $this->definition['require-dev'][$dependency] ?? null;
+
+        if (!is_string($constraint)) {
+            throw new DefinitionException(sprintf('Could not get version constraint for dev dependency "%s".', $dependency));
+        }
+
+        return $constraint;
+    }
+
+    public function save(): void
+    {
+        $encoded = json_encode($this->definition, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        if (!is_string($encoded)) {
+            throw new DefinitionException(sprintf('Could not encode definition due to "%s".', json_last_error_msg()));
+        }
+
+        file_put_contents($this->getPackageDirectory() . DIRECTORY_SEPARATOR . 'composer.json', $encoded);
     }
 }
